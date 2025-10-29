f042a4dc67ccd15852ba7d2144fe4f39f027dea4
igbekele best <bwale974@gmail.com>
2025-10-29 20:24:32 +0100

    Update urls.py

--- countries/urls.py
+++ countries/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import CountryViewSet
-
-router = DefaultRouter()
-router.register(r'countries', CountryViewSet, basename='country')
+from . import views
+from rest_framework.routers import DefaultRouter  # if you still use router for other endpoints, optional
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("status/", views.top_level_status, name="status"),
+    path("countries/refresh/", views.refresh_countries, name="countries-refresh"),
+    path("countries/image/", views.countries_image, name="countries-image"),
+    path("countries/", views.list_countries, name="countries-list"),
+    path("countries/<str:name>/", views.get_country, name="countries-detail"),
+    path("countries/<str:name>/delete/", views.delete_country, name="countries-delete"),
 ]
